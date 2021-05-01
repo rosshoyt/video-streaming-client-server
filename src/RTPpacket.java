6e9a2fae915c8ced0d2a724219f9a1c5fbd260b3
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-05-01 14:37:06 -0700

    Fill the RTP header fields in ctor, changed RTPpacket.java

--- src/RTPpacket.java
+++ src/RTPpacket.java
@@ -43,17 +43,24 @@ public class RTPpacket{
         TimeStamp = Time;
         PayloadType = PType;
 
-        //build the header bistream:
+        //build the header bitstream:
         //--------------------------
         header = new byte[HEADER_SIZE];
 
-        //.............
-        //TO COMPLETE
-        //.............
         //fill the header array of byte with RTP header fields
 
-        //header[0] = ...
-        // .....
+        header[0] = (byte)(Version << 6 | Padding << 5 | Extension << 4 | CC);
+        header[1] = (byte)(Marker << 7 | PayloadType & 0x000000FF);
+        header[2] = (byte)(SequenceNumber >> 8);
+        header[3] = (byte)(SequenceNumber & 0xFF);
+        header[4] = (byte)(TimeStamp >> 24);
+        header[5] = (byte)(TimeStamp >> 16);
+        header[6] = (byte)(TimeStamp >> 8);
+        header[7] = (byte)(TimeStamp & 0xFF);
+        header[8] = (byte)(Ssrc >> 24);
+        header[9] = (byte)(Ssrc >> 16);
+        header[10] = (byte)(Ssrc >> 8);
+        header[11] = (byte)(Ssrc & 0xFF);
 
 
         //fill the payload bitstream:
@@ -62,9 +69,7 @@ public class RTPpacket{
         payload = new byte[data_length];
 
         //fill payload array of byte from data (given in parameter of the constructor)
-        //......
-
-        // ! Do not forget to uncomment method printheader() below !
+       payload = data;
 
     }
 
