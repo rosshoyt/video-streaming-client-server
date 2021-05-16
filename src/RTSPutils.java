5b24cc743a975bbd38651e62d6370793c60d70dd
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-05-15 17:00:32 -0700

    Added support for RTSP status codes 200, 404 to RTSPutils.java

--- src/RTSPutils.java
+++ src/RTSPutils.java
@@ -2,10 +2,11 @@ public class RTSPutils {
     // Line delimiter used in RTSP messages TODO make public
     private final static String CRLF = "\r\n";
 
+
     /**
      * Method that creates a custom RTSP Response from a server to client
-     * TODO support additional RTSP status codes
-     * @param statusCode int RTSP status code (404 = file not found, 401 = unauthorized, etc)
+     * Supported RTSP Status Codes: 200 OK, 404 file not found, 401 not authorized, 501 Not Implemented
+     * @param statusCode int RTSP status code
      * @param rtspSeqNum int sequence number
      * @param rtspID int id
      * @return String RTSP response with  the requested params
@@ -14,13 +15,13 @@ public class RTSPutils {
         StringBuffer sb = new StringBuffer();
         // Write status line
         sb.append("RTSP/1.0 ");
-        switch (statusCode){
-            case 401:
-                sb.append(statusCode + " ERR" + CRLF);
-                break;
-            case 501: // Client request code NOT IMPLEMENTED
-                sb.append(statusCode + " ERR" + CRLF);
+
+        if(statusCode == 200){
+            sb.append(statusCode + " OK" + CRLF);
+        }else if(statusCode == 401 || statusCode == 404 || statusCode ==  501){
+            sb.append(statusCode + " ERR" + CRLF);
         }
+
         // set sequence number and session id lines
         sb.append("CSeq: "+rtspSeqNum+CRLF);
         sb.append("Session: "+rtspID+CRLF);
