f6395be993b1a02b9dfbb81718fd729cb7ba7240
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-05-15 23:42:46 -0700

    Use AudioInputStream in AudioStream.java

--- src/AudioStream.java
+++ src/AudioStream.java
@@ -1,28 +1,66 @@
-import java.io.FileInputStream;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * Class which allows an audio file to be packetized for audio streaming
+ */
 public class AudioStream {
-    FileInputStream fis; //audio file
-    int audio_sample_nb; //current audio sample number
+    AudioInputStream ais; // input stream
+    File file; // the underlying audio file
 
-    public AudioStream(String filename) throws FileNotFoundException{
-        fis = new FileInputStream(filename);
-        audio_sample_nb = 0;
+    int audio_sample_nb = 0; //current audio sample number
+
+    private boolean isValid = false; // true only if no problems occured setting up the audio input stream
+
+    double timerRateMS = -1; // time length (MS) that the user will be requesting audio frames at
+
+    //AudioFileFormat audioFileFormat = new AudioFileFormat(AudioFileFormat.Type.WAVE,);
+    /**
+     * Creates an audio stream of a specified folder
+     * @param filename
+     * @param timerRateMS
+     * @throws IOException
+     * @throws UnsupportedAudioFileException
+     */
+    public AudioStream(String filename, double timerRateMS) throws IOException, UnsupportedAudioFileException {
+        this.timerRateMS = timerRateMS;
+        file = new File(filename);
+        try{
+            ais = AudioSystem.getAudioInputStream(file);
+            isValid = true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
-    public int getnextchunk(byte[] chunk) throws IOException {
+    /**
+     * Reads the next frame of audio data from the audio stream into a byte array
+     * @param frame the byte array to fill with audio data
+     * @return
+     * @throws IOException
+     */
+    public int getnextframe(byte[] frame) throws IOException {
+
         int length = 0;
-        String length_string;
-        byte[] chunk_length = new byte[5];
+        // TODO set length
+        //ais.read(chunk_length, 0, 5);
 
+        return (ais.read(frame,0, length));
+    }
 
-        fis.read(chunk_length, 0, 5);
+    public boolean isValid() {
+        return isValid;
+    }
 
-        length_string = new String(chunk_length);
-        length = Integer.parseInt(length_string);
-
-        return (fis.read(chunk,0, length));
+    public void setValid(boolean valid) {
+        isValid = valid;
     }
 
 }
